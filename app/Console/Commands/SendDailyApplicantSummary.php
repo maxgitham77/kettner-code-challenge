76ce4d21bc89b2567c2fa1b0a064477e7a390b73
Maxwell IG <m.imasuen-goertzen@perfect-media-solutions.de>
2025-05-15 21:41:58 +0200

    schedule daily email summary of applications to admin

---
+++ app/Console/Commands/SendDailyApplicantSummary.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class SendDailyApplicantSummary extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:send-daily-applicant-summary';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        //
+    }
+}
